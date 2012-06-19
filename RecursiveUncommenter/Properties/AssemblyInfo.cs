eb30060dd46915c7ca416755868ead07674bd973
David Bronleewe <david@bronleewe.net>
2012-06-19 13:29:51 -0500

    Updated projects and assembly info.

--- RecursiveUncommenter/Properties/AssemblyInfo.cs
+++ RecursiveUncommenter/Properties/AssemblyInfo.cs
@@ -1,10 +1,8 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("uncommenter")]
+[assembly: AssemblyTitle("uncomment")]
 [assembly: AssemblyCompany("David Bronleewe")]
-[assembly: AssemblyProduct("RecursiveUncommenter")]
+[assembly: AssemblyProduct("Recursive Uncommenter")]
 [assembly: AssemblyCopyright("© 2012 David Bronleewe")]
-[assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
